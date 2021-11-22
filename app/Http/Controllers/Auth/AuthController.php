a33314ba24e116aa5ff4c9d46cf2499bb72c655f
Obada Alzidi <opadaalziede@gmail.com>
2021-11-22 20:24:40 +0200

    init migration files

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register() {
+
+        // TODO 
+    }
+
+    public function login() {
+
+        // TODO 
+    }
+
+    public function logout() {
+        
+        // TODO 
+    }
+}
---
+++ database/migrations/2021_11_22_182007_create_trips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('starts_at');
+            $table->unsignedBigInteger('governorate_id');
+            $table->text('details');
+            $table->integer('number_of_seats');
+            $table->double('estimated_time');
+            $table->string('car_plate');
+            $table->double('cost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
